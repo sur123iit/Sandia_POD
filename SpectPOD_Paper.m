9dcc5be862acbdb7b9d87dc46b4216a80540f1fa
sur123iit <33849596+sur123iit@users.noreply.github.com>
2017-11-26 19:15:53 -0500

    got the SPOD modes

--- SpectPOD_Paper.m
+++ SpectPOD_Paper.m
@@ -5,29 +5,66 @@
 %Check what factor needs to be multiplied to ffts
 %expand later for others.
 clear all;
-load('\\citrixvdi.osg.ufl.edu\citrix-student$\UserData\surabhi123iit\Documents\Downloads\spect8_1.mat');
-[x1,x2,N] = size(spect8fluc);
 Nf = 386;
 n = Nf/2;
-Nb = 100; %hardcoded
-eigval1 = zeros(n,1);
-eigval2 = zeros(n,1);
-%Only applying for frequency, f = 1
-for f = 1:n 
-qj_bar_inst = zeros(x1*2,Nb);
-for i = 1:Nb
+Nb = 280; %hardcoded
+qj_bar_ens = zeros(6360,n,Nb);%not sure about its dimensions
+%add spect8_1.mat into your system
+load('\\citrixvdi.osg.ufl.edu\citrix-student$\UserData\surabhi123iit\Documents\Downloads\spect8_1.mat');
+for i = 1:100
     u_fluc_inst(:,:) = spect8fluc(:,3,((386*(i-1))+1):(386*i));
     v_fluc_inst(:,:) = spect8fluc(:,4,((386*(i-1))+1):(386*i));
     qj = vertcat(u_fluc_inst,v_fluc_inst);
     clearvars u_fluc_inst v_fluc_inst;
     qj_bar = fft(qj');
     clear qj;
-    qj_bar_inst(:,i) = qj_bar(f,:);
+    qj_bar_t = qj_bar';
+    clearvars qj_bar;
+    qj_bar_ens(:,:,i) = qj_bar_t(:,1:193);
+    clearvars qj_bar_t;
 end
+clearvars spect8fluc;
+%add spect8_2.mat into your system
+load('\\citrixvdi.osg.ufl.edu\citrix-student$\UserData\surabhi123iit\Documents\Downloads\spect8_2.mat');
+for i = 1:100
+    u_fluc_inst(:,:) = spect8fluc(:,3,((386*(i-1))+1):(386*i));
+    v_fluc_inst(:,:) = spect8fluc(:,4,((386*(i-1))+1):(386*i));
+    qj = vertcat(u_fluc_inst,v_fluc_inst);
+    clearvars u_fluc_inst v_fluc_inst;
+    qj_bar = fft(qj');
+    clear qj;
+    qj_bar_t = qj_bar';
+    clearvars qj_bar;
+    qj_bar_ens(:,:,i+100) = qj_bar_t(:,1:193);
+    clearvars qj_bar_t;
+end
+clearvars spect8fluc;
+%add spect8_3.mat into your system
+load('\\citrixvdi.osg.ufl.edu\citrix-student$\UserData\surabhi123iit\Documents\Downloads\spect8_3.mat');
+for i = 1:80
+    u_fluc_inst(:,:) = spect8fluc(:,3,((386*(i-1))+1):(386*i));
+    v_fluc_inst(:,:) = spect8fluc(:,4,((386*(i-1))+1):(386*i));
+    qj = vertcat(u_fluc_inst,v_fluc_inst);
+    clearvars u_fluc_inst v_fluc_inst;
+    qj_bar = fft(qj');
+    clear qj;
+    qj_bar_t = qj_bar';
+    clearvars qj_bar;
+    qj_bar_ens(:,:,i+200) = qj_bar_t(:,1:193);
+    clearvars qj_bar_t;
+end
+clearvars spect8fluc;
+%Modify this section...
+POD_spect_coeff = zeros(6360,Nb,n);%calculate size of this variable here
+eigen_val = zeros(Nb,Nb,n);
+for f = 1:n
+qj_bar_inst(:,:) = qj_bar_ens(:,1,:);
 qj_mat = (conj(qj_bar_inst))'*qj_bar_inst;
 [eigvec,eigval] = svd(qj_mat);
-eigval1(f,1) = eigval(1,1);
-eigval2(f,1) = eigval(2,2);
-%check what this might be a function of.
-%POD_spect_coeff = qj_bar_inst*eigvec*1/sqrt(eigval); 
+eigen_val(:,:,f) = eigval;
+POD_spect_inst = qj_bar_inst*eigvec*1/sqrt(eigval);
+clearvars eigvec eigval;
+POD_spect_coeff(:,:,f) = POD_spect_inst;
+clearvars POD_spect_inst;
 end
+
