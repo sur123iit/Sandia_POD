471a68fd9e8abbbb82acc346b4ae7f3aaa45fb06
sur123iit <33849596+sur123iit@users.noreply.github.com>
2018-03-28 16:22:12 -0400

    update convergence code

--- Compare_Spac_Cont15000.m
+++ Compare_Spac_Cont15000.m
@@ -20,14 +20,13 @@ plot(1:40,abs(Phi_dot(1:40)),'o-');
 pbaspect([ 1 1 1]);
 %%
 uv = load('Y:\rawdata\Sandia_cavity\Denoise velocity data\tr_POD\Mach0.8\vel_fluc.txt');
-Ns3 = 15000;
+Ns3 = 19300;
 vel_fluc3 = uv(:,1:Ns3);
 [eigvec3, eigval3, PODcoeff3, Phi3] = POD_Snapshot(vel_fluc3);
 %%
 Phi_dot = dot(Phi2(:,1:200),Phi3(:,1:200));
 plot(1:40,abs(Phi_dot(1:40)),'o-');
 %%
-<<<<<<< HEAD
 grid_mat = (Phi3(:,1:200))'*(Phi2(:,1:200));
 grid_mat = abs(grid_mat);
 c = gray(30);
@@ -40,7 +39,24 @@ colorbar;
 set(gcf,'Position',[0 0 800 800])
 xticks(0:20:100), yticks(0:20:100)
 %%
-
-=======
->>>>>>> 8aacf305c1c58b26ef144b730111f1bfad924026
+clearvars vel_fluc;
+load('Y:\rawdata\Sandia_cavity\Vel_incrN.mat');
+clearvars vel_mat vel_mean
+%%
+u3(:,:) = vel_fluc(:,3,:);
+v3(:,:) = vel_fluc(:,4,:);
+vel_fluc4 = vertcat(u3,v3);
+%%
+[eigvec4, eigval4, PODcoeff4, Phi4] = POD_Snapshot(vel_fluc4);
+%%
+grid_mat = (Phi3(:,1:200))'*(Phi4(:,1:200));
+grid_mat = abs(grid_mat);
+c = gray(30);
+c = flipud(c);
+colormap(c);
+h = pcolor(grid_mat(1:100,1:100));
+caxis([0 1]);
+pbaspect([1 1 1])
+colorbar;
+set(gcf,'Position',[0 0 800 800])
 
