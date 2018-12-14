7741e2b04f67bfaba169b1ac9c45bbd6b2ee8e49
sur123iit <33849596+sur123iit@users.noreply.github.com>
2018-12-14 17:25:34 -0500

    update December 14

---
+++ VarToRms.m
@@ -0,0 +1,10 @@
+clear all;
+[L,D,Ma,Uinf,Fs,N,Nb] = load_parameters(1);
+folderName = 'Y:\rawdata\Sandia_cavity\SpectralVelocityReconstructions\RossiterModes\RossiterMode2\';
+fileName = 'uv_rec_var.txt';
+completeName = strcat(folderName,fileName);
+uv_var = load(completeName);
+uv_var1 = uv_var*N;
+uv_rms1 = sum(uv_var1,2);
+uv_rms1 = uv_rms1/(N*Nb);
+uv_rms = sqrt(uv_rms1);
